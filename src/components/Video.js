87f6041290a41180f5c4bfb93a8c5741b7519e80
aidenzegil <40921524+aidenzegil@users.noreply.github.com>
2022-08-01 10:23:38 -0400

    first commit

---
+++ src/components/Video.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Video = () => {
+    return (
+        <div id="video">
+            {/* <iframe src="https://thisvideo"></iframe> */}
+        </div>
+    )
+}
+
+export default Video
